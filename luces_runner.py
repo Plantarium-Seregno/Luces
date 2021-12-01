993706d80ace8f351325e35a196ac0791f9a6397
Carlo Stemberger <carlo.stemberger@gmail.com>
2021-12-01 10:41:10 +0100

    Managing opening hours

--- luces_runner.py
+++ luces_runner.py
@@ -6,9 +6,11 @@ This automates Luces, by managing the hours and a playlist.
 Copyright © 2020-21, Plantarium Società Agricola
 """
 
+import time
 import datetime
 import pathlib
 import astral
+import humanized_opening_hours as hoh
 import subprocess
 
 
@@ -17,21 +19,26 @@ home = str(pathlib.Path.home())
 log = 'luces_runner.log'
 media_dir = f'{home}/src/Luces/Media'
 
+location = astral.Location(('Seregno', 'Lombardy', 45.64677, 9.22676,
+    'Europe/Rome', 0))
+opening_hours = hoh.OHParser(
+        'Mo-Fr 09:00-12:30,14:00-19:30; Sa 09:00-19:30; Su 09:00-19:00')
+
 def get_hour():
     return datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=1)))
 
 
-location = astral.Location(('Seregno', 'Lombardy', 45.64677, 9.22676,
-    'Europe/Rome', 0))
+def get_day():
+    return datetime.date.today()
+
+
+def get_sunset():
+    return location.sun(date=today, local=True)['sunset']
+
+
 now = get_hour()
-today = datetime.date.today()
-sunset = location.sun(date=today, local=True)['sunset']
-if today.weekday() < 7:
-    closing_hour = datetime.datetime.combine(today, datetime.time(19, 20),
-            datetime.timezone(datetime.timedelta(hours=1)))
-else:
-    closing_hour = datetime.datetime.combine(today, datetime.time(18, 50),
-            datetime.timezone(datetime.timedelta(hours=1)))
+today = get_day()
+sunset = get_sunset()
 
 with open(f'{media_dir}/luces.list', 'r') as playlist:
     projects = [project.strip() for project in playlist.readlines()]
@@ -54,22 +61,29 @@ def write_log(message):
         f.write(f'----- {str(datetime.datetime.now())}: {message}\n')
 
 
-while now < sunset:
-    write_log('PAUSE')
-    with open(log, 'a') as f:
-        subprocess.run([f'{home}/midi_file_player.py',
-            'src/Luces/Media/Silenzio.mid'], stdout=f, stderr=f)
-    write_log('END PAUSE')
-    now = get_hour()
+while True:
+    if today != get_day(): # once a day, calculate sunset time
+        today = get_day()
+        sunset = get_sunset()
 
-while now < closing_hour:
-    project = get_project()
-    write_log(f'SHOW: {project}')
-    with open(log, 'a') as f:
-        subprocess.run([f'{home}/src/Luces/luces.py', media_dir, project],
-                stdout=f, stderr=f)
-    with open(log, 'a') as f:
-        subprocess.run([f'{home}/midi_file_player.py',
-            'src/Luces/Media/Silenzio.mid'], stdout=f, stderr=f)
-    write_log('END SHOW')
-    now = get_hour()
+    if opening_hours.is_open():
+        if now < sunset:
+            write_log('PAUSE')
+            with open(log, 'a') as f:
+                subprocess.run([f'{home}/midi_file_player.py',
+                    'src/Luces/Media/Silenzio.mid'], stdout=f, stderr=f)
+            write_log('END PAUSE\n')
+            now = get_hour()
+        else:
+            project = get_project()
+            write_log(f'SHOW: {project}')
+            with open(log, 'a') as f:
+                subprocess.run([f'{home}/src/Luces/luces.py', media_dir,
+                    project], stdout=f, stderr=f)
+            with open(log, 'a') as f:
+                subprocess.run([f'{home}/midi_file_player.py',
+                    'src/Luces/Media/Silenzio.mid'], stdout=f, stderr=f)
+            write_log('END SHOW\n')
+            now = get_hour()
+    else:
+        time.sleep(1) # wait 1 second
